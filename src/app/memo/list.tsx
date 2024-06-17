17c4b2ead82b8bd440152887f54309c45e1c97bb
Bsomac <bsoma.k0904@gmail.com>
2024-06-17 21:09:12 +0900

    Create delete

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,16 +1,20 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import { router, useNavigation } from "expo-router";
 import MemoListItem from "../../components/MemoListItem";
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import LogoutButton from "../../components/LogOutButton";
+import { db, auth } from "../../config";
+import { type Memo } from "../../../types/memo";
 
 const handlePress = (): void => {
   router.push("memo/create");
 };
 
 const List = (): JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([]);
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
@@ -18,14 +22,33 @@ const List = (): JSX.Element => {
         return <LogoutButton />;
       },
     });
-  });
+  }, []);
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+    const q = query(ref, orderBy("updatedAt", "desc"));
+    const unsubscibe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = [];
+      snapshot.forEach((doc) => {
+        const { bodyText, updatedAt } = doc.data();
+        remoteMemos.push({
+          id: doc.id,
+          bodyText,
+          updatedAt,
+        });
+      });
+      setMemos(remoteMemos);
+    });
+    return unsubscibe;
+  }, []);
   return (
     <View style={styles.container}>
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
+      <FlatList
+        data={memos}
+        renderItem={({ item }) => <MemoListItem memo={item} />}
+      />
       <CircleButton onPress={handlePress}>
         <Icon name="plus" size={24} color="#ffffff" />
       </CircleButton>
